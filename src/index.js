2940cb52a0929152847f9201422f8082ae8baf7c
andre ferrer <andrerferrer@hotmail.com>
2021-06-26 06:28:42 -0300

    Finish lovecode

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello from src/index.js!");
+// 1 select the element
+const btn = document.querySelector('#click-me');
+// 2 add event listener
+// btn.addEventListener('event', CALLBACK)
+btn.addEventListener('click', (event) => {
+  // 3 implement the callback
+  // console.log(event);
+  // console.log(event.currentTarget);
+  // console.log(event.currentTarget === btn);
+  event.currentTarget.innerText = 'Clicked! Wait please...';
+});
